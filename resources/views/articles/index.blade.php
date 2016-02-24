da0bd89c2333a6a8f25c63c8fc2f42cd888ebf91
David Jarrin <dmjarrin@gmail.com>
2016-02-23 20:33:46 -0500

    saving progress

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -3,13 +3,13 @@
 @section('content');
     <h1>Articles</h1>
 
-    {{--@foreach($articles as $article)--}}
-        {{--<article>--}}
-            {{--<h2>{{ $article->title }}</h2>--}}
+    @foreach($articles as $article)
+        <article>
+            <h2>{{ $article->title }}</h2>
 
-            {{--<div class="body">{{ $article->body }}</div>--}}
+            <div class="body">{{ $article->body }}</div>
 
-        {{--</article>--}}
+        </article>
 
-    {{--@endforeach--}}
+    @endforeach
 @stop
